b5338ac71b00369c18b08d946ac779cc44d3b2b3
xinyan999 <xin.yan.vic@gmail.com>
2023-06-30 21:28:37 +1000

    cypress test 4th commit

--- cypress/e2e/secondTest.spec.js
+++ cypress/e2e/secondTest.spec.js
@@ -3,7 +3,8 @@ describe('Test log out', () => {
     cy.loginToApplication()
   })
 
-  it('verify log out successfully', {retries:2}, {browser: 'chrome'}, () => {
+  // it('verify log out successfully', {retries:2}, {browser: 'chrome'}, () => {
+  it('verify log out successfully', {retries:2}, () => {
     cy.contains(' Settings ').click()
     cy.contains(' Or click here to logout. ').click()
     cy.get('.navbar-nav').should('contain', ' Sign in ')
